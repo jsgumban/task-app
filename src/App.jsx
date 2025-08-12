30483ef03585a0da54630d8645e839db99ef565f
jsgumban <gumban.johnhel@gmail.com>
2025-08-13 01:04:04 +0800

    Initial setup: initialize vite + react, add bootstrap, add basic html structure

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div className="container">
+          <a className="navbar-brand" href="#">Task App</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        </div>
+      </nav>
+      
+      <div className="container mt-4">
+        <div className="row">
+          <div className="col-12">
+            <h1>Task App</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
